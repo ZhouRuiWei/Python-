2c11005dcaeef76dc9bc46e771411f3c075efd2c
ZhouRuiWei <zhouruiwei96@foxmail.com>
2016-04-03 17:13:48 +0800

    Added files via upload
    贴吧只看楼主模式，把楼主发布的内容保存到本地文件。程序仅仅针对于发表普通文本内容，不包含各种七七八八的情况（例如百度会员啊图片啊之类的）

---
+++ tieba2.py
@@ -0,0 +1,41 @@
+#-*- coding: utf-8 -*-
+import urllib2,re
+
+tiezi = raw_input('请输入帖子地址：')
+def geturl(i):
+    url =tiezi+'?see_lz=1'+'&pn='+str(i)
+    req = urllib2.urlopen(url).read().decode('utf-8')
+    return req
+
+def getpagenumber(req):
+    pat3 = re.compile(r'<span class="red">(\d*)</span>')
+    number = pat3.findall(req)
+    return number
+
+def gettitle(req):
+    pat1 = re.compile(r'<title>(.*?)</title>')
+    longtitle =pat1.findall(req)[0].split('_')
+    title = '-'.join(longtitle[:-2])
+    return title
+
+	
+def getcontent(req):
+    pat2 = re.compile(r'id="post_content.*?>(.*?)</div>')
+    txt = pat2.findall(req,re.S)
+    return txt
+ 
+tempreq = geturl(1)   
+number = getpagenumber(tempreq)[0]
+print number
+title = gettitle(tempreq)
+f = open(title+'.txt','w')
+for i in range(int(number)+1):
+    req = geturl(i)
+    txt = getcontent(req)
+    for eachitem in txt:
+        f.write(eachitem.replace('<br>',''))
+        f.write('\n')
+f.close()
+
+
+
